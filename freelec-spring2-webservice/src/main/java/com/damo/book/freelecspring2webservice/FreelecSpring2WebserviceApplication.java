13b30fb46042366a7cfa15f45be4d2322fc3e737
Damho Lee <ydh4745@gmail.com>
2023-04-28 10:39:18 +0900

    Initial commit

---
+++ freelec-spring2-webservice/src/main/java/com/damo/book/freelecspring2webservice/FreelecSpring2WebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.damo.book.freelecspring2webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelecSpring2WebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelecSpring2WebserviceApplication.class, args);
+	}
+
+}
