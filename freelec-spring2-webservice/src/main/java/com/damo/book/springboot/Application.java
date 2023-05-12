007afdc653b37c90837196841dd3812a8b7446e6
Damho Lee <ydh4745@gmail.com>
2023-05-12 10:03:17 +0900

    혼자한거 최종

--- freelec-spring2-webservice/src/main/java/com/damo/book/springboot/Application.java
+++ freelec-spring2-webservice/src/main/java/com/damo/book/springboot/Application.java
@@ -2,7 +2,7 @@ package com.damo.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+//import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 //@EnableJpaAuditing
 @SpringBootApplication
